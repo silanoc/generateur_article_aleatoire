98dbd50a526356edbee4b07e72178c6190aaed19
silanoc <silanoc@gmail.com>
2021-11-01 20:03:07 +0100

    creation du fichier, dépendances interne

---
+++ controle_console.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python3
+# coding: utf-8
+
+import modele
+import vue_console as vue
+
+"""
+controle
+pour console
+"""
+
+
+def main():
+    vue.intro()
+
+if __name__ == "__main__":
+    main()
