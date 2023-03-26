a7ca7d9557fdb6c4dbd38fa0fa104b66fe72f342
silanoc <silanoc@gmail.com>
2023-03-26 16:39:27 +0200

    menage

--- modele.py
+++ modele.py
@@ -16,16 +16,14 @@ class articlepresse():
     
     def __init__(self, text):
         """initilisation
-        
         arg: 
             une chaine de texte (si possible longue)
         return :
             aucun
         """
-        #Déclaration de varable 
-        self.texte = text #le texte sur lequel on travail
-        self.dicostatique = {} #pour la fonction liste_et_compte_mots
-        self.dicodoublons = {} #pour la fonction cherche_binomes_mots - ce qui est recherché
+        self.texte:str = text #le texte sur lequel on travail
+        self.dicostatique:dict = {} #pour la fonction liste_et_compte_mots
+        self.dicodoublons:dict = {} #pour la fonction cherche_binomes_mots - ce qui est recherché
     
     def retirer_ponctuation(self, txt_a_nettoyer:str) -> str:
         """ La première version du logiciel est sommaire. Pour se simplifier la vie, il faut supprimer tous les signe de ponctuations.
@@ -45,29 +43,28 @@ class articlepresse():
         #quand on supprime un : par exemple, cela fait deux espace. Remplacer ces artefacts de cagage
         for _ in range(len(self.texte)):
             txt_a_nettoyer = txt_a_nettoyer.replace("  ", " ")
+        for _ in range(len(self.texte)):
+            txt_a_nettoyer = txt_a_nettoyer.replace(" ", " ")
         return txt_a_nettoyer
 
-    def liste_et_compte_mots(self,texte_a_traiter):
+    def liste_et_compte_mots(self, texte_a_traiter:str) -> list[list,dict]:
         """Compte le nombre d'occurence d'un mot.
         Fonction créée un peu par erreur, mais elle peut être utile pour faire des statistiques.
-        
         arg : 
-            self
+            texte_a_traiter (str)
         return : 
-            [liste[mot1,mot2,,...],dictionnaire {mot:nb,...}]
-        #>>> liste_et_compte_mots("le petit chat de béatrice est sur le petit mur du jardin de Yves")
-        #['le','petit','chat','de','béatrice','est','sur','le','petit','mur','du','jardin','de','Yves']
-        #[['le','petit','chat','de','béatrice','est','sur','le','petit','mur','du','jardin','de','Yves'],{'le':2,'petit':2,'chat':1,'de':2,'béatrice':1,'est':1,'sur':1,'mur':1,'du':1,'jardin':1,'Yves':1}]
-        """
-        self.liste_mot = texte_a_traiter.split(" ")
-        for mot in self.liste_mot:
-            if mot in self.dicostatique:
-                self.dicostatique[mot] += 1
+            2 object dans un liste. La liste des mots de la chaine et le comptrage de chaque mot dans un dictionnaire
+            """
+        liste_mot: list[str] = texte_a_traiter.split(" ")
+        dicostatistique = {}
+        for mot in liste_mot:
+            if mot in dicostatistique:
+                dicostatistique[mot] += 1
             else:
-                self.dicostatique[mot] = 1
-        return [self.liste_mot, self.dicostatique]
+                dicostatistique[mot] = 1
+        return [liste_mot, dicostatistique]
     
-    def cherche_binomes_mots(self):
+    def cherche_binomes_mots(self, texte_a_traiter:str) -> dict:
         """la fonction principale de l'objet : faire un dictionnaire de fréquences des mots qui se suivent.
         
         arg :
@@ -75,35 +72,45 @@ class articlepresse():
         return :
             dictionnaire {mot1:[mot2, mot3, mot3, mot4],...}
         """
-        liste_mots_suivant = []
+        liste_mots_suivant:list = []
+        dicodoublons:dict = {}
         #recherche des espaces délimitants les mots
         list_position_espace = []
-        for i in range(len(self.texte)):
-            if self.texte[i] == " ":
+        for i in range(len(texte_a_traiter)):
+            if texte_a_traiter[i] == " ":
                 list_position_espace.append(i)
         #print(list_position_espace)
         #recherche doublons mot
         debut1 = 0
         for i in range(len(list_position_espace)-2): #AFAIRE : ATTENTION ça ne prends pas les deux derniers mot. A vérifier.
             fin1 = list_position_espace[i]
-            fin2 = list_position_espace[i+1]
+            fin2 = list_position_espace[i + 1]
             #print(debut1, fin1,fin2)
-            mot1 = self.texte[debut1:fin1]
-            mot2 = self.texte[fin1:fin2]
+            mot1 = texte_a_traiter[debut1:fin1]
+            mot2 = texte_a_traiter[fin1:fin2]
             debut1 = list_position_espace[i]
             #print(mot1,mot2)
-            liste_mots_suivant.append([mot1,mot2])
+            liste_mots_suivant.append([mot1, mot2])
         #print(liste_mots_suivant)
         #fait un dictionnaire avec toutes les occurences possible après un même mot.
         #les doublons sont normaux, cela veut dire que le mot revient plusieurs fois, cela correspond au calcul de leur fréquence   
         for j in range(len(liste_mots_suivant)):
-            if liste_mots_suivant[j][0] in self.dicodoublons.keys():
+            if liste_mots_suivant[j][0] in dicodoublons.keys():
                 #print('doublons')
-                self.dicodoublons[liste_mots_suivant[j][0]].append(liste_mots_suivant[j][1])
+                dicodoublons[liste_mots_suivant[j][0]].append(liste_mots_suivant[j][1])
             else:
                 #print('nouveau')
-                self.dicodoublons[liste_mots_suivant[j][0]]=[(liste_mots_suivant[j][1])]   
-
+                dicodoublons[liste_mots_suivant[j][0]]=[(liste_mots_suivant[j][1])]
+        return dicodoublons 
+    
+    def tout_enchainer(self):
+        self.texte = self.retirer_ponctuation(self.texte)
+        #self.dicostatique = self.liste_et_compte_mots(self.texte)[1]
+        #print(self.dicostatique)
+        #print("--------------")
+        self.dicodoublons = self.cherche_binomes_mots(self.texte)
+        print(self.dicodoublons)
+        
 
 class articleauhasard():
     """génére un texte aléatoire à partir d'un dictionnaire
@@ -137,27 +144,11 @@ class articleauhasard():
     def genereruntexte(self):
         """intier avec choixmotpourcommencer(), puis enchainer chercherlemotsuivant() """
         pass
-
-
-def testarticledepresse():
-    """3 versions de tailles différentes"""
-    #enchainement logique
-    a = articlepresse(textetest2)
-    a.texte = a.retirer_ponctuation(a.texte)
-    a.liste_et_compte_mots(a.texte)
-    print(a.dicostatique)
-    #a.cherche_binomes_mots()
-    #print(a.dicodoublons)
-
-
-def testarticleauhasard():
-    # 5 dictionnaire issues des phrases de test via testarticledepresse
-    pass
    
 
 def main():
-    testarticledepresse()
-    #testarticleauhasard()
+    a = articlepresse(textetest2)
+    a.tout_enchainer()
     
 
 if __name__ == "__main__":
--- tests/test_modele.py
+++ tests/test_modele.py
@@ -10,3 +10,22 @@ class Test_modele():
         textdebase = articlepresse(chaine)
         retirer = textdebase.retirer_ponctuation(chaine)
         assert retirer == "Le petit chat d Hercule est mort "
+        
+    def test_liste_et_compte_mots(self):
+        chaine = "le petit chat de béatrice est sur le petit mur du jardin de Yves"
+        textdebase = articlepresse(chaine)
+        list_mot = textdebase.liste_et_compte_mots(chaine)[0]
+        dico_mot = textdebase.liste_et_compte_mots(chaine)[1]
+        assert list_mot == ['le','petit','chat','de','béatrice','est','sur','le','petit','mur','du','jardin','de','Yves']
+        assert dico_mot == {'le':2,'petit':2,'chat':1,'de':2,'béatrice':1,'est':1,'sur':1,'mur':1,'du':1,'jardin':1,'Yves':1}
+        
+    def test_cherche_binomes_mots(self):
+        chaine = "le petit chat de béatrice est sur le petit mur du jardin de Yves"
+        textdebase = articlepresse(chaine)
+        dico = textdebase.cherche_binomes_mots(chaine)
+        assert dico == {' le': [' petit', ' petit'], ' petit':[' chat', ' mur'], ' chat': [' de'], ' de': [' béatrice', ' Yves'], ' béatrice': [' est'],
+                        ' est': [' sur'], ' sur': [' le'], ' mur': [' du'], ' du': [' jardin'], ' jardin': [' de'], ' Yves': []}
+        
+        
+
+        
