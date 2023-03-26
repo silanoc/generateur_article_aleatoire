ba2f547167b5451fa84f781425b2df66fb564b4d
silanoc <silanoc@gmail.com>
2023-03-26 15:13:30 +0200

    premier menage, externatlisation de texte

---
+++ tests/test_modele.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python3
+# coding: utf-8
+
+from modele import *
+
+class Test_modele():
+    
+    def test_retirer_ponctuation(self):
+        retirer = articlepresse.retirer_ponctuation(self)
+        pass
